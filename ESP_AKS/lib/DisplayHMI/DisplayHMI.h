8185523c56b447af09f396924cd6ce8a97a2ba8d
incubation-0 <incubation@tutamail.com>
2026-04-20 14:02:37 +0300

    Update DisplayHMI.h
    begin() içindeki nextion mesajlarını ve bckmd=0 komutunu algılaısn diye eklenmiştir

--- ESP_AKS/lib/DisplayHMI/DisplayHMI.h
+++ ESP_AKS/lib/DisplayHMI/DisplayHMI.h
@@ -5,10 +5,11 @@ class DisplayHMI {
    private:
     bool HMI_isInitialized;
     void HMI_sendEndBytes();
+    void HMI_drainRxBuffer();
 
    public:
     DisplayHMI();
     bool begin();
-    void updateScreen(uint8_t HMI_speed, uint8_t HMI_battery);
+    void updateScreen(uint16_t HMI_speed, uint8_t HMI_battery);
     bool readTouchCommand(uint8_t& HMI_command);
 };
